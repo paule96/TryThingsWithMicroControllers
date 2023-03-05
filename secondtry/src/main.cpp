a960b8fc6d7f2ec709b13b6be0e64ebcec7567e5
Dirk Liebscher <diki@umdd.de>
2023-03-05 17:13:34 +0100

    3rdtry
    Co-authored-by: paule96 <paul-jeschke@outlook.com>

--- secondtry/src/main.cpp
+++ secondtry/src/main.cpp
@@ -1,64 +1,88 @@
 #include <Arduino.h>
 
-int LED_2 = 2;
-int LED_3 = 3;
-int LED_4 = 4;
-int LED_5 = 5;
-int LED_6 = 6;
-int LED_7 = 7;
-int LED_8 = 8;
-int LED_9 = 9;
-int LED_10 = 10;
+int Buzzer = 14;
+int PWMChannel = 0;
 
-void setup(){
-    pinMode(LED_2, OUTPUT);
-    pinMode(LED_3, OUTPUT);
-    pinMode(LED_4, OUTPUT);
-    pinMode(LED_5, OUTPUT);
-    pinMode(LED_6, OUTPUT);
-    pinMode(LED_7, OUTPUT);
-    pinMode(LED_8, OUTPUT);
-    pinMode(LED_9, OUTPUT);
-    pinMode(LED_10, OUTPUT);
-    printf("Ich bin an");
+/* This is interesting:
+If you use sizeof it will return the bytes that is used by the object you pass in as a parameter.
+Because objects can have different size, this is not a good method to count objects in an array.
+To count objects in the array you must devide the size of the array,
+by the size of a single entry in the array. There is no builtin way in C++ todo it better.
+This will always work, because an array can only contains elements from the same type.
+If you want to programm very efficently, define the length of the array in a const and
+pass it in by the array initialisation, like so:
+note_t song[7] = {........};
+
+Then you know for sure your array will always be 7 items long.
+*/
+uint getLength(int array[])
+{
+    return sizeof(array) / sizeof(array[0]);
 }
 
-void loop(){
-    digitalWrite(LED_2, HIGH);
-    delay(200);
-    digitalWrite(LED_3, HIGH);
-    delay(200);
-    digitalWrite(LED_4, HIGH);
-    delay(200);
-    digitalWrite(LED_5, HIGH);
-    delay(200);
-    digitalWrite(LED_6, HIGH);
-    delay(200);
-    digitalWrite(LED_7, HIGH);
-    delay(200);
-    digitalWrite(LED_8, HIGH);
-    delay(200);
-    digitalWrite(LED_9, HIGH);
-    delay(200);
-    digitalWrite(LED_10, HIGH);
-    delay(200);
-    digitalWrite(LED_2, LOW);
-    delay(200);
-    digitalWrite(LED_3, LOW);
-    delay(200);
-    digitalWrite(LED_4, LOW);
-    delay(200);
-    digitalWrite(LED_5, LOW);
-    delay(200);
-    digitalWrite(LED_6, LOW);
-    delay(200);
-    digitalWrite(LED_7, LOW);
-    delay(200);
-    digitalWrite(LED_8, LOW);
-    delay(200);
-    digitalWrite(LED_9, LOW);
-    delay(200);
-    digitalWrite(LED_10, LOW);
-    delay(200);
+uint getLength(note_t array[])
+{
+    return sizeof(array) / sizeof(array[0]);
+}
 
+int leds[] = {2, 3, 4, 5, 6, 7, 8};
+note_t song[] = {NOTE_C, NOTE_D, NOTE_E, NOTE_F, NOTE_G, NOTE_A, NOTE_B};
+
+int songLength = getLength(song);
+
+void setup()
+{
+    for (int i = 0; i < getLength(leds); i++)
+    {
+        printf("Active pin: %.1f", i);
+        pinMode(leds[i], OUTPUT);
+    }
+
+    pinMode(Buzzer, OUTPUT);
+    ledcAttachPin(Buzzer, PWMChannel);
+}
+
+void loop()
+{
+    for (int octave = 4; octave < 8; octave++)
+    {
+        for (int i = 0; i < songLength; i++)
+        {
+            printf("play note: %.1f from the song", i);
+            switch (song[i])
+            {
+            case NOTE_C:
+                digitalWrite(leds[0], HIGH);
+                break;
+            case NOTE_D:
+                digitalWrite(leds[1], HIGH);
+                break;
+            case NOTE_E:
+                digitalWrite(leds[2], HIGH);
+                break;
+            case NOTE_F:
+                digitalWrite(leds[3], HIGH);
+                break;
+            case NOTE_G:
+                digitalWrite(leds[4], HIGH);
+                break;
+            case NOTE_A:
+                digitalWrite(leds[5], HIGH);
+                break;
+            case NOTE_B:
+                digitalWrite(leds[6], HIGH);
+                break;
+            default:
+                // TODO: mach fehler
+                break;
+            }
+            ledcWriteNote(PWMChannel, song[i], octave);
+            delay(300);
+            for (size_t l = 0; l < getLength(leds); l++)
+            {
+                printf("Set pin %.1f to low.", l);
+                digitalWrite(leds[l], LOW);
+            }
+        }
+    }
 }
