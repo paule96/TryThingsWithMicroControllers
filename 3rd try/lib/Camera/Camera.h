528ee49fb660829a96019a646074e2b8c02d391a
Paul Jeschke <Paul-Jeschke@outlook.com>
2023-03-11 21:41:31 +0000

    try to fix the code

--- 3rd try/lib/Camera/Camera.h
+++ 3rd try/lib/Camera/Camera.h
@@ -1,8 +1,8 @@
 void SetupCamera();
 char* GetCameraUi();
-class Frame
+struct Frame
 {
-public:
+// public:
     // Frame(const size_t &jpg_buf_len, uint8_t *jpg_buf);
     size_t _jpg_buf_len = 0;
     uint8_t *_jpg_buf = NULL;
