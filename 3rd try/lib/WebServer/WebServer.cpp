41a827e2b015b328a73e7352c49da4a6170d3bcc
Paul Jeschke <Paul-Jeschke@outlook.com>
2023-03-11 19:35:44 +0000

    try to fix webserver

--- 3rd try/lib/WebServer/WebServer.cpp
+++ 3rd try/lib/WebServer/WebServer.cpp
@@ -8,13 +8,15 @@ FS& filesystem = GetCurrentMount();
  */
 static const char *TAG = "camera_httpd";
 
+AsyncWebServer server(80);
+
 /**
  * @brief starts a webserver to serve the camerui
 */
 void StartCameraServer()
 {
     ESP_LOGI(TAG, "Starting web server on port: '80'");
-    AsyncWebServer server(80);
+
     filesystem = GetCurrentMount();
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
               {
