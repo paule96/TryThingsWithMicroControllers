447465efe18eeda3235ce23947ff0ab75e92bec7
Paul Jeschke <Paul-Jeschke@outlook.com>
2023-03-11 20:51:59 +0000

    try to fix work with buffers

--- 3rd try/lib/WebServer/WebServer.cpp
+++ 3rd try/lib/WebServer/WebServer.cpp
@@ -20,24 +20,24 @@ static const char *_STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %
 /// @param request the request object, that will get an answer
 void StreamCameraFeed(AsyncWebServerRequest *request)
 {
-  AsyncWebServerResponse *response = request->beginChunkedResponse(_STREAM_CONTENT_TYPE, [](uint8_t *buffer, size_t maxLen, size_t index) -> size_t {
+  AsyncWebServerResponse *response = request->beginChunkedResponse(_STREAM_CONTENT_TYPE, [](uint8_t *buffer, size_t maxLen, size_t index) -> size_t
+                                                                   {
   //Write up to "maxLen" bytes into "buffer" and return the amount written.
   //index equals the amount of bytes that have been already sent
   //You will be asked for more data until 0 is returned
   //Keep in mind that you can not delay or yield waiting for more data!
+  Serial.printf("Starting stream frame. Max length: %x, ", maxLen);
     Frame frame = GetCameraStream();
+    Serial.printf("Frame length: %x", frame._jpg_buf_len);
+    Serial.println();
     size_t maxBufferRead;
     if(maxLen > frame._jpg_buf_len){
       maxBufferRead = frame._jpg_buf_len;
     }else{
       maxBufferRead = maxLen;
     }
-    for (size_t i = 0; i < maxBufferRead; i++)
-    {
-      buffer[i] = frame._jpg_buf[i];
-    }
-    return GetCameraStream()._jpg_buf_len;
-  });
+    memccpy(buffer, &frame._jpg_buf, 0, maxBufferRead);
+    return maxBufferRead; });
 }
 
 /**
@@ -54,4 +54,3 @@ void StartCameraServer()
   server.begin();
   ESP_LOGI(TAG, "Starting stream server on port: '80'");
 }
-
